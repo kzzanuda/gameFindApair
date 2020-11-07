ac619ad2afcb115990c4ca4ef6e1540cff7d401b
Kirill Schebina <kirsscheb98@gmail.com>
2020-11-07 10:52:14 +0400

    Update player.js
    Более адекватный подсчет очков

--- js/player.js
+++ js/player.js
@@ -8,15 +8,14 @@ let Player = {
     this._score = 0;
   },
   setNewScore: function() {
-    if ( 15 * oGame.getLvl() - this._lvlTime > Math.exp(oGame.getLvl()/1.3) * 2 ) {
-      this._timeScore = 15 * oGame.getLvl() - this._lvlTime;
+    let scoreByTime = 15 * oGame.getLvl() - this._lvlTime;
+    if ( scoreByTime > Math.exp(oGame.getLvl()/1.3) * 2 ) {
+      this._timeScore += scoreByTime * 100;
       console.log(true);
     } else {
-      this._timeScore = -Math.exp(oGame.getLvl()/1.3) * 2;
+      this._timeScore += -Math.exp(oGame.getLvl()/1.3) * 200;
     }
-    this._score += Math.ceil(this._timeScore + Math.log(oGame.getLvl()) * 2) * 100 ;
-    console.log(this._timeScore * 100);
-    console.log( Math.exp(oGame.getLvl()/2) * 200);
+    this._score += Math.ceil(this._timeScore + (Math.log(oGame.getLvl()) * 2) * 200) ;
   },
   getScore: function() {
     return this._score;
